cf1f224f9e7c4891ecf91c03979160ed7132a037
Tal Arbatov <talarbatov98@gmail.com>
2019-03-28 15:44:48 +0200

    finished github basic oauth skeleton

--- server/app.js
+++ server/app.js
@@ -10,6 +10,39 @@ const app = express();
 
 app.use('/', express.static(publicPath));
 
+
+var githubOAuth = require('github-oauth')({
+  githubClient: process.env.CLIENT_ID,
+  githubSecret: process.env.GITHUB_SECRET,
+  baseURL: 'http://localhost:' + PORT,
+  loginURI: '/auth/github',
+  callbackURI: '/auth/github/callback'
+})
+
+app.get("/auth/github", function(req, res){
+  console.log("started oauth");
+  return githubOAuth.login(req, res);
+});
+
+app.get("/auth/github/callback", function(req, res){
+  console.log("received callback");
+  return githubOAuth.callback(req, res);
+});
+
+githubOAuth.on('error', function(err) {
+  console.error('there was a login error', err)
+})
+
+githubOAuth.on('token', function(token, serverResponse) {
+  console.log('HEEHEHEHEHEHHHEHEHE')
+  serverResponse.end(JSON.stringify(token))
+})
+
+app.get('/test', (req,res) => {
+  res.send('HELLO!');
+})
+
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 })
